a2adb1967864036004d11766cb36a6506d488f19
Erko Bridee <erko.bridee@gmail.com>
2021-03-25 10:36:51 +0100

    feat(docs): add js doc comment on useDidUpdate

--- src/useDidUpdate/useDidUpdate.ts
+++ src/useDidUpdate/useDidUpdate.ts
@@ -2,7 +2,14 @@ import * as React from 'react';
 
 import { TFunction } from '@erkobridee/ts-definitions-common';
 
-// like componentDidUpdate
+/**
+ * This react hook will execute when like the component did update
+ *
+ * In case of passing and array of dependencies, this will executes only one of them gets updated
+ *
+ * @param {TFunction} fn
+ * @param {React.DependencyList} deps - optional
+ */
 export const useDidUpdate = (fn: TFunction, deps?: React.DependencyList) => {
   const firstRunRef = React.useRef(true);
 
