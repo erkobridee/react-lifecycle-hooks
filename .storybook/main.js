ec9c7c4caa72149d1c47312a13597ff31a205572
Erko Bridee <erko.bridee@gmail.com>
2021-03-25 09:59:56 +0100

    update(storybook): main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -12,7 +12,11 @@
 // };
 
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../src/index.stories.tsx',
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   addons: [
     '@storybook/addon-postcss',
     '@storybook/addon-links',
